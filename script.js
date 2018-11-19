15d62b1b5888e4e352cf72a72e6d94c180dd5dd0
Roger Yau <rogerpuichungyau@gmail.com>
2018-11-19 07:34:04 +0000

    class refactoring

--- script.js
+++ script.js
@@ -86,107 +86,32 @@ function initMap() {
     
     
     // beize curve ---------------------------------------------------------------------------------------------
+    
     var path =  [{lat: 22.324624, lng: 114.172305}, {lat: 23.927836, lng:121.086477}]
-    var bezierPath = getLatLngPath( path[0], path[1] );
+    let HKG_TW = new createBeizeCurve(path[0],path[1])
+    // console.log(HKG_TW.beizePath)
+    // var bezierPath = getLatLngPath( path[0], path[1] );
     // console.log(bezierPath)
-    
-    
-    function getLatLngPath( p0, p1 )
-    {
-        var handles = getBezierHandles( p0, p1 );
-
-        var points = [ p0, handles.a, handles.b, p1 ];
-
-        var path = [];
-
-        var sections = 40;
-
-        for( var t = 0; t <= sections; ++t )
-        {
-            var b = getBezier( points, t / sections );
-
-            path.push( new google.maps.LatLng( b.lat, b.lng ) );
-        }
-
-        return path;
-    }
-    
-    function getBezierHandles( p0, p1 )
-    {
-        // a, b points are perpendicular to q1, q3 which are on line connecting p0 and p1.
-        // slope between two points : ( y2 - y1 ) / (x2 - x1 ).
-        // to get perpendicular slope use, reciprocal slope function: -(x2 - x1 ) / ( y2 - y1 )
-        // curvature is distance between q1 and a; q3 and b;
-        //
-        //        a              b
-        //        |              |
-        //        |              |
-        // p0-----q1-----q2-----q3-----p1
-
-        var q2 = { lat: (p0.lat + p1.lat) / 2, lng: (p0.lng + p1.lng) / 2 };
-        var q1 = { lat: (p0.lat + q2.lat) / 2, lng: (p0.lng + q2.lng) / 2 };
-        var q3 = { lat: (q2.lat + p1.lat) / 2, lng: (q2.lng + p1.lng) / 2 };
-
-        var dLat = p1.lat - p0.lat;
-        var dLng = p1.lng - p0.lng;
-
-        var curvature = 0.15;
-
-        if( p0.lng > p1.lng )
-        {
-            return {
-                a: { lat: q1.lat + ( -dLng * curvature ), lng: q1.lng + ( dLat * curvature ) },
-                b: { lat: q3.lat + ( -dLng * curvature ), lng: q3.lng + ( dLat * curvature ) }
-            };
-        }
-        else
-        {
-            return {
-                a: { lat: q1.lat - ( -dLng * curvature ), lng: q1.lng + ( dLat * curvature ) },
-                b: { lat: q3.lat - ( -dLng * curvature ), lng: q3.lng + ( dLat * curvature ) }
-            };
-        }
-    }
-    
-    function getBezier( points, t )
-    {
-        var ab = larp( points[ 0 ], points[ 1 ], t );
-        var bc = larp( points[ 1 ], points[ 2 ], t );
-        var cd = larp( points[ 2 ], points[ 3 ], t );
-        var abbc = larp( ab, bc, t );
-        var bccd = larp( bc, cd, t );
-
-        return larp( abbc, bccd, t );
-    }
-    
-    
-    function larp( a, b, t )
-    {
-        return { lat: a.lat + ( b.lat - a.lat ) * t, lng: a.lng + ( b.lng - a.lng ) * t };
-    }
     // beize curve ----------------------------------------------------------------------------------------------
-    
-    
-    
-    
-    
     
     
     // Create the polyline and add the symbol to it via the 'icons' property.
     // var path =  [{lat: 22.324624, lng: 114.172305}, {lat: 23.927836, lng:121.086477}]
     var line = new google.maps.Polyline({
+      path: HKG_TW.beizePath,
       // path: path,
-      path:bezierPath,
+      // path:bezierPath,
       // strokeColor:'#a68974',
       // strokeColor:'#FF4B4B',
-      strokeColor:'#d0dbd7',
+      // strokeColor:'#d0dbd7',
+      strokeColor:'#b9c1d1',
       // strokeColor: '#0099FF',
       // 	strokeColor:'#f6dda7',
       	 //strokeColor:'#82a18f',
       	 
       strokeOpacity: 1,
       // strokeOpacity:0,
-      strokeWeight: 1,
+      strokeWeight: 1.5,
       geodesic: true,
       icons: [{
         // icon:icons.parking.icon,
@@ -202,10 +127,12 @@ function initMap() {
       map: map
     });
     
-
     
+    // line.strokeColor = '#f6dda7'
     animateCircle(line);
+    // console.log(line)
     
+    // repeated symbols animated on the bezierPath line
     function animateCircle(line) {
         var count = 0;
         window.setInterval(function() {
@@ -216,44 +143,41 @@ function initMap() {
             // icons[0].offset = (count / 2) + '%';
             icons[0].offset = (count) + '%';
             line.set('icons', icons);
-            
-
-            
         }, 40);
         // console.log(line)
         
     }
     
     
-    var departure = new google.maps.Marker({
-      map:map,
-      position: path[0],
-      icon:{
-        // path: "M-20,0a20,20 0 1,0 40,0a20,20 0 1,0 -40,0",
-        // path:'M0-48c-9.8 0-17.7 7.8-17.7 17.4 0 15.5 17.7 30.6 17.7 30.6s17.7-15.4 17.7-30.6c0-9.6-7.9-17.4-17.7-17.4z',
-        // path:"M2 12C2 6.48 6.48 2 12 2s10 4.48 10 10-4.48 10-10 10S2 17.52 2 12zm10 6c3.31 0 6-2.69 6-6s-2.69-6-6-6-6 2.69-6 6 2.69 6 6 6z",
-        fillColor: '#ff0000',
-        fillOpacity: .6,
-        // anchor: new google.maps.Point(0,0),
-        strokeWeight: 0,
-        scale: 0.5
-      }
-    });
+    // var from_point = new google.maps.Marker({
+    //   map:map,
+    //   position: path[0],
+    //   icon:{
+    //     // path: "M-20,0a20,20 0 1,0 40,0a20,20 0 1,0 -40,0",
+    //     // path:'M0-48c-9.8 0-17.7 7.8-17.7 17.4 0 15.5 17.7 30.6 17.7 30.6s17.7-15.4 17.7-30.6c0-9.6-7.9-17.4-17.7-17.4z',
+    //     // path:"M2 12C2 6.48 6.48 2 12 2s10 4.48 10 10-4.48 10-10 10S2 17.52 2 12zm10 6c3.31 0 6-2.69 6-6s-2.69-6-6-6-6 2.69-6 6 2.69 6 6 6z",
+    //     fillColor: '#ff0000',
+    //     fillOpacity: .6,
+    //     // anchor: new google.maps.Point(0,0),
+    //     strokeWeight: 0,
+    //     scale: 0.5
+    //   }
+    // });
     
-    var destination = new google.maps.Marker({
-      map:map,
-      position: path[1],
-      icon:{
-        // path: "M-20,0a20,20 0 1,0 40,0a20,20 0 1,0 -40,0",
-        // path:'M0-48c-9.8 0-17.7 7.8-17.7 17.4 0 15.5 17.7 30.6 17.7 30.6s17.7-15.4 17.7-30.6c0-9.6-7.9-17.4-17.7-17.4z',
-        // path:"M2 12C2 6.48 6.48 2 12 2s10 4.48 10 10-4.48 10-10 10S2 17.52 2 12zm10 6c3.31 0 6-2.69 6-6s-2.69-6-6-6-6 2.69-6 6 2.69 6 6 6z",
-        fillColor: '#ff0000',
-        fillOpacity: .6,
-        // anchor: new google.maps.Point(0,0),
-        strokeWeight: 0,
-        scale: 0.5
-      }
-    });
+    // var to_point = new google.maps.Marker({
+    //   map:map,
+    //   position: path[1],
+    //   icon:{
+    //     // path: "M-20,0a20,20 0 1,0 40,0a20,20 0 1,0 -40,0",
+    //     // path:'M0-48c-9.8 0-17.7 7.8-17.7 17.4 0 15.5 17.7 30.6 17.7 30.6s17.7-15.4 17.7-30.6c0-9.6-7.9-17.4-17.7-17.4z',
+    //     // path:"M2 12C2 6.48 6.48 2 12 2s10 4.48 10 10-4.48 10-10 10S2 17.52 2 12zm10 6c3.31 0 6-2.69 6-6s-2.69-6-6-6-6 2.69-6 6 2.69 6 6 6z",
+    //     fillColor: '#ff0000',
+    //     fillOpacity: .6,
+    //     // anchor: new google.maps.Point(0,0),
+    //     strokeWeight: 0,
+    //     scale: 0.5
+    //   }
+    // });
     
     
 
